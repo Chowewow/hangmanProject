70b0d4da92f4773b852fddadfc956fccbb88f7d5
Chowewow <chowewoow@gmail.com>
2022-05-13 00:18:19 +0800

    added user profile tab

--- app/routes.py
+++ app/routes.py
@@ -68,4 +68,8 @@ def guest():
     return redirect(next_page)
 
     
-    
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
